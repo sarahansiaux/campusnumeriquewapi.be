b7fcb3dd0c69d9ae6f0792479b92b567f12b6ce7
Benjamin Geets <benjamingeets@tutamail.com>
2021-11-16 19:05:29 +0100

    nique zebi

--- .svelte-kit/dev/generated/manifest.js
+++ .svelte-kit/dev/generated/manifest.js
@@ -2,6 +2,9 @@ const c = [
 	() => import("../../../src/routes/__layout.svelte"),
 	() => import("../components/error.svelte"),
 	() => import("../../../src/routes/index.svelte"),
+	() => import("../../../src/routes/conditions-dutilisation.svelte"),
+	() => import("../../../src/routes/protection-des-donnees.svelte"),
+	() => import("../../../src/routes/mentions-legales.svelte"),
 	() => import("../../../src/routes/actualites.svelte")
 ];
 
@@ -11,8 +14,17 @@ export const routes = [
 	// src/routes/index.svelte
 	[/^\/$/, [c[0], c[2]], [c[1]]],
 
+	// src/routes/conditions-dutilisation.svelte
+	[/^\/conditions-dutilisation\/?$/, [c[0], c[3]], [c[1]]],
+
+	// src/routes/protection-des-donnees.svelte
+	[/^\/protection-des-donnees\/?$/, [c[0], c[4]], [c[1]]],
+
+	// src/routes/mentions-legales.svelte
+	[/^\/mentions-legales\/?$/, [c[0], c[5]], [c[1]]],
+
 	// src/routes/actualites.svelte
-	[/^\/actualites\/?$/, [c[0], c[3]], [c[1]]]
+	[/^\/actualites\/?$/, [c[0], c[6]], [c[1]]]
 ];
 
 // we import the root layout/error components eagerly, so that
--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="text-center mt-auto py-10">
-    <p>Condition générales d’utilisation - Mentions légales - Charte de protection des données</p>
+    <p><a href="/conditions-dutilisation">Condition générales d’utilisation</a> - <a href="/mentions-legales">Mentions légales</a> - <a href="/protection-des-donnees">Charte de protection des données</a></p>
     <p>Réalisé par des étudiants d'e-business</p>
 </footer>
 <style>
