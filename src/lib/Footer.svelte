0b5b86cebac907e23738921114b737987317f500
Benjamin Geets <benjamingeets@tutamail.com>
2021-11-21 15:04:38 +0100

    progress

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="text-center mt-auto py-10">
-    <p><a href="/conditions-dutilisation">Condition générales d’utilisation</a> - <a href="/mentions-legales">Mentions légales</a> - <a href="/protection-des-donnees">Charte de protection des données</a></p>
+    <p><a class="hover:text-rouge transition" href="/conditions-dutilisation">Condition générales d’utilisation</a> - <a class="hover:text-rouge transition" href="/mentions-legales">Mentions légales</a> - <a class="hover:text-rouge transition" href="/protection-des-donnees">Charte de protection des données</a></p>
     <p>Réalisé par des étudiants d'e-business</p>
 </footer>
 <style>
